b7427dcbdb6e29fc6becf4d5b3c3f8d0df3d4231
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2020-11-02 19:52:26 +0800

    Set ui scale to 1.0 to prevent unexpected scaling.

--- src/main/java/art/cctcc/c1635/antsomg/demo/Main.java
+++ src/main/java/art/cctcc/c1635/antsomg/demo/Main.java
@@ -98,6 +98,7 @@ public class Main extends PApplet {
     }
 
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
         PApplet.main(Main.class);
     }
 }
